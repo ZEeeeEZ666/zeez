f9f5d8132c15f237cf8aa83d10422dcabf57949f
ZEeeeEZ666 <85716847+ZEeeeEZ666@users.noreply.github.com>
2023-04-01 20:08:24 +0300

    Add files via upload

---
+++ App.py
@@ -0,0 +1,21 @@
+import time
+
+print("wsp")
+time.sleep(1)
+print("wyd?")
+
+r1 = input()
+
+if r1 == "No" or "no":
+    time.sleep(.5)
+    print("why bro")
+
+r2 = input()
+
+if r2 == "طيزك انت مال امك" or "طيزك" or "زوبري":
+    time.sleep(1)
+    print("خخخخخخ")
+    time.sleep(1.5)
+    print("انت خدت عليا اوي")
+    time.sleep(2)
+    print("يلا غور يا خول شوف حد ينيكك")
